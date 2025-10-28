96517f08af1cafe05eb261498f6847899707d8d9
Zawuza <andreizawuza@gmail.com>
2025-10-28 22:55:19 +0100

    Work on monotonicity_correct

--- examples.v
+++ examples.v
@@ -1,3 +1,4 @@
+From Coq Require Import List QArith Lia Lqa.
 From Coquelicot Require Import Coquelicot.
 From Verinncoq Require Import matrix_extensions neuron_functions real_subsets 
                               real_subsets_instances piecewise_affine
@@ -10,7 +11,7 @@ Section Monotonicity1DHyperpropery.
 
 Definition monotonicity (nn: TPWANNSequential (RSOPM:=Q_RSOPMD)): Prop :=
     forall x1 x2,
-        toRS x1 <= (toRS x2) = true -> toRS (nn_eval nn x2) <= toRS (nn_eval nn x1) = false.
+        toRS x1 <= (toRS x2) = true -> toRS (nn_eval nn x1) <= toRS (nn_eval nn x2) = true.
 
 Definition W_monotonicity: ConvexPolyhedron 2 :=
     Polyhedron (RSOPM:=Q_RSOPMD) 2 (cons (Constraint 2 [[1], [- (1)]] 0) nil).
@@ -22,7 +23,7 @@ Definition NNDH_monotonicity: NNHyperproperty :=
         2 2 W_monotonicity (LinearTPWAF Mone (null_vector 2)) 
         (LinearTPWAF netSat_monotonicity_M (null_vector 1)).
 
-Lemma monotonicity_validation:
+Lemma monotonicity_correct:
     forall (nn: TPWANNSequential (RSOPM:=Q_RSOPMD)),
         monotonicity nn <-> nn_satisfies_nndh nn NNDH_monotonicity.
 Proof.
@@ -31,8 +32,98 @@ Proof.
       unfold nn_satisfies_nndh.
       unfold NNDH_monotonicity.
       intros x HxW.
-      unfold tpwaf_eval at 2 3.
-      unfold pwaf_eval, pwaf_eval_helper.
+      pose proof (colvec_split 1 1 x) as Hsplit.
+      destruct Hsplit as [x1 [x2 [Hx1 [Hx2 Hxconcat]]]].
+      specialize (H x1 x2). simpl in H.
+      unfold tpwaf_eval; simpl.
+      unfold netSat_monotonicity_M.
+      rewrite Mplus_null_vector.
+      unfold Mmult, toRS.
+      unfold coeff_colvec.
+      rewrite (coeff_mat_bij _ _ 0 0); try lia.
+      unfold sum_n, sum_n_m, Iter.iter_nat; simpl.
+      unfold coeff_mat at 1; simpl.
+      rewrite (mult_zero_l (K:=Q_RSOPMD)).
+      unfold coeff_mat at 1; simpl.
+      rewrite (mult_zero_l (K:=Q_RSOPMD)).
+      repeat rewrite (plus_zero_l (G:=Q_RSOPMD)).
+      unfold coeff_mat at 1; simpl.
+      unfold colvec_concat at 1.
+      unfold Mplus at 1; rewrite coeff_mat_bij; try lia.
+      unfold extend_colvec_at_bottom at 1, mk_colvec at 1; rewrite coeff_mat_bij; try lia; simpl.
+      unfold extend_colvec_on_top at 1, mk_colvec at 1; rewrite coeff_mat_bij; try lia; simpl.
+      unfold colvec_concat at 1, coeff_colvec at 1.
+      unfold Mplus at 1; rewrite coeff_mat_bij; try lia.
+      unfold extend_colvec_at_bottom at 1, mk_colvec at 1; rewrite coeff_mat_bij; try lia; simpl.
+      unfold extend_colvec_on_top at 1; unfold mk_colvec at 2; rewrite coeff_mat_bij; try lia; simpl.
+      rewrite (plus_zero_l (G:=Q_RSOPMD)).
+      rewrite (plus_zero_r (G:=Q_RSOPMD)).
+      assert (Hhelp: forall v, v = x1 -> nn_eval nn v = nn_eval nn x1). intros v Hv; rewrite Hv; reflexivity.
+      rewrite Hhelp; clear Hhelp.
+      - unfold coeff_mat at 1; simpl.
+        rewrite (mult_one_l (K:=Q_RSOPMD)).
+        unfold colvec_concat at 1.
+        unfold Mplus at 1; rewrite coeff_mat_bij; try lia.
+        unfold extend_colvec_at_bottom at 1, mk_colvec at 1; rewrite coeff_mat_bij; try lia; simpl.
+        unfold extend_colvec_on_top at 1; unfold mk_colvec at 1; rewrite coeff_mat_bij; try lia; simpl.
+        rewrite (plus_zero_l (G:=Q_RSOPMD)).
+        rewrite (plus_zero_r (G:=Q_RSOPMD)).
+        unfold coeff_colvec at 2, colvec_concat at 1.
+        unfold Mplus at 1; rewrite coeff_mat_bij; try lia.
+        unfold extend_colvec_at_bottom at 1, mk_colvec at 1; rewrite coeff_mat_bij; try lia; simpl.
+        unfold extend_colvec_on_top at 1, mk_colvec at 1; rewrite coeff_mat_bij; try lia; simpl.
+        rewrite (plus_zero_l (G:=Q_RSOPMD)).
+        unfold coeff_colvec at 2, colvec_concat at 1.
+        unfold Mplus at 1; rewrite coeff_mat_bij; try lia.
+        unfold extend_colvec_at_bottom at 1, mk_colvec at 1; rewrite coeff_mat_bij; try lia; simpl.
+        unfold extend_colvec_on_top at 1, mk_colvec at 3; rewrite coeff_mat_bij; try lia; simpl.
+        rewrite (plus_zero_r (G:=Q_RSOPMD)).
+        assert (Hhelp: forall v, v = x2 -> nn_eval nn v = nn_eval nn x2). intros v Hv; rewrite Hv; reflexivity.
+        rewrite (Hhelp (mk_colvec 1 _)); clear Hhelp.
+        * unfold mult, plus; simpl.
+          assert (Halgebra: forall x11 x12 x21 x22,
+                                x11 = x12 ->
+                                x21 = x22 ->
+                                QDEP_le x11 x21 = true ->
+                                QDEP_le QDEP_zero (QDEP_plus (QDEP_mult (QDEP_opp QDEP_one) x12) x22) = true). {
+                                    intros x11 x12 x21 x22 Hx11 Hx21.
+                                    rewrite Hx11, Hx21.
+                                    unfold QDEP_le.
+                                    admit.
+                                }
+          apply (Halgebra (toRS (nn_eval nn x1)) _ (toRS (nn_eval nn x2)) _).
+          - unfold toRS; reflexivity.
+          - unfold toRS; reflexivity.
+          - apply H.  
+            rewrite Hxconcat in HxW.
+            unfold in_convex_polyhedron, W_monotonicity in HxW.
+            specialize (HxW (Constraint 2 [[1], [- (1)]] 0) (in_eq _ _)).
+            unfold satisfies_lc in HxW.
+            admit.
+        * rewrite Hx2.
+          unfold mk_colvec.
+          apply mk_matrix_ext; intros i j Hi Hj.
+          destruct i; destruct j; try lia.
+          unfold coeff_colvec.
+          unfold Mplus; repeat (rewrite coeff_mat_bij; try lia).
+          do 2 rewrite (plus_zero_r (G:=Q_RSOPMD)).
+          unfold Mone; repeat (rewrite coeff_mat_bij; try lia); simpl.
+          rewrite (mult_one_l (K:=Q_RSOPMD)).
+          rewrite (mult_zero_l (K:=Q_RSOPMD)).
+          rewrite (plus_zero_l (G:=Q_RSOPMD)).
+          reflexivity.        
+      - rewrite Hx1.
+        unfold mk_colvec.
+        apply mk_matrix_ext; intros i j Hi Hj.
+        destruct i; destruct j; try lia.
+        unfold coeff_colvec.
+        unfold Mplus; repeat (rewrite coeff_mat_bij; try lia).
+        do 2 rewrite (plus_zero_r (G:=Q_RSOPMD)).
+        unfold Mone; repeat (rewrite coeff_mat_bij; try lia); simpl.
+        rewrite (mult_one_l (K:=Q_RSOPMD)).
+        rewrite (mult_zero_l (K:=Q_RSOPMD)).
+        rewrite (plus_zero_r (G:=Q_RSOPMD)).
+        reflexivity.      
     *
 Admitted.
 
