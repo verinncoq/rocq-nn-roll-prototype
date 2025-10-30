1d708a72f2e48ca8a372925392acf385a73d2221
Zawuza <andreizawuza@gmail.com>
2025-10-30 23:07:27 +0100

    Finish examples: - Prove correctness of 1d monotonicity NNDH - Add test NNs

---
+++ example.v
@@ -0,0 +1,321 @@
+From Coq Require Import List QArith Reals Lia Lqa Lra.
+From Coquelicot Require Import Coquelicot.
+From Verinncoq Require Import matrix_extensions neuron_functions real_subsets 
+                              real_subsets_instances piecewise_affine
+                              NNDH neural_networks NNDH_to_fme fourier_motzkin fm_q_support.
+
+Open Scope RSOPM_scope.
+Import RealSubsetNotations.
+
+Section Monotonicity1DHyperpropery.
+
+Definition is_monotone_1d (nn: TPWANNSequential (RSOPM:=Q_RSOPMD)): Prop :=
+    forall x1 x2,
+        toRS x1 <= (toRS x2) = true -> toRS (nn_eval nn x1) <= toRS (nn_eval nn x2) = true.
+
+Definition W_monotonicity_1d: ConvexPolyhedron 2 :=
+    Polyhedron (RSOPM:=Q_RSOPMD) 2 (cons (Constraint 2 [[1], [- (1)]] 0) nil).
+
+Lemma W_monotonicity_1d_correct:
+    forall x1 x2,
+       toRS x1 <= (toRS x2) = true <-> in_convex_polyhedron (colvec_concat x1 x2) W_monotonicity_1d.
+Proof.
+    intros x1 x2; split; intro H.
+    * unfold in_convex_polyhedron, W_monotonicity_1d.
+      intros constraint Hconstraint.
+      unfold In in Hconstraint.
+      destruct Hconstraint as [Hconstraint|Hconstraint]; try contradiction Hconstraint.
+      rewrite <- Hconstraint.
+      unfold satisfies_lc.
+      unfold dot, Mmult; rewrite coeff_mat_bij; try lia.
+      unfold sum_n, sum_n_m, Iter.iter_nat; simpl.
+      unfold transpose, colvec_concat, Mplus, extend_colvec_at_bottom, extend_colvec_on_top, mk_colvec.
+      repeat (rewrite coeff_mat_bij; try lia); simpl.
+      unfold coeff_colvec, coeff_mat, coeff_Tn, fst; simpl.
+      rewrite (mult_one_l (K:=Q_RSOPMD)), (plus_zero_l (G:=Q_RSOPMD)), (plus_zero_r (G:=Q_RSOPMD)).
+      rewrite (plus_zero_r (G:=Q_RSOPMD)), <- (opp_mult_m1 (K:=Q_RSOPMD)).
+      rewrite (ax_real_leq_true Q_RSOPMD) in H.
+      apply Rle_minus in H; unfold Rminus in H.
+      rewrite <- (ax_opp_is_opp Q_RSOPMD), <- (ax_real_plus Q_RSOPMD), <- (ax_zero_is_zero Q_RSOPMD) in H.
+      rewrite <- (ax_real_leq_true Q_RSOPMD) in H.
+      apply H.
+    * unfold in_convex_polyhedron, W_monotonicity_1d in H.
+      specialize (H (Constraint 2 [[1], [- (1)]] 0) (in_eq _ _)).
+      unfold satisfies_lc in H.
+      unfold dot, Mmult in H; rewrite coeff_mat_bij in H; try lia.
+      unfold sum_n , sum_n_m, Iter.iter_nat in H; simpl in H.
+      unfold transpose, colvec_concat, Mplus, extend_colvec_at_bottom, extend_colvec_on_top, mk_colvec in H.
+      repeat (rewrite coeff_mat_bij in H; try lia); simpl in H.
+      unfold coeff_colvec, coeff_mat, coeff_Tn, fst in H; simpl in H.
+      rewrite (mult_one_l (K:=Q_RSOPMD)), (plus_zero_l (G:=Q_RSOPMD)), (plus_zero_r (G:=Q_RSOPMD)) in H.
+      rewrite (plus_zero_r (G:=Q_RSOPMD)), <- (opp_mult_m1 (K:=Q_RSOPMD)) in H.
+      rewrite (ax_real_leq_true Q_RSOPMD).
+      apply Rminus_le; unfold Rminus.
+      rewrite <- (ax_opp_is_opp Q_RSOPMD), <- (ax_real_plus Q_RSOPMD), <- (ax_zero_is_zero Q_RSOPMD).
+      rewrite <- (ax_real_leq_true Q_RSOPMD).
+      apply H.
+Qed.
+
+Definition netSat_monotonicity_1d_M : matrix (T:=T Q_RSOPMD) 1 4 := [[0, 0, - (1), 1]].
+
+Definition NNDH_monotonicity_1d: NNHyperproperty :=
+    NNDH (nn_in_dim:=1) (nn_out_dim:=1)
+        2 2 W_monotonicity_1d (LinearTPWAF Mone (null_vector 2)) 
+        (LinearTPWAF netSat_monotonicity_1d_M (null_vector 1)).
+
+Definition monotonicity_1d_postcondition_helper {RSOPM}:
+    matrix (T:=T RSOPM) (2 + 2 * 1) 1 -> colvec (RSOPM:=RSOPM) 4.
+Proof.
+    intros H.
+    unfold colvec; apply H.
+Defined.
+
+Lemma monotonicity_1d_postcondition:
+    forall (x1: colvec 1) (x2: colvec 1) (x: colvec 2) (nn: TPWANNSequential (input_dim:=1) (output_dim:=1)),
+        x1 = mk_colvec 1 (fun i : nat => coeff_colvec 0 x i) ->
+        x2 = mk_colvec 1 (fun i : nat => coeff_colvec 0 x (i + 1)) ->
+        x = colvec_concat x1 x2 ->
+        (toRS (nn_eval nn x1) <= toRS (nn_eval nn x2)) =
+        (0 <=
+            toRS 
+                (tpwaf_eval (LinearTPWAF netSat_monotonicity_1d_M (null_vector 1))
+                    (monotonicity_1d_postcondition_helper
+                    (colvec_concat (tpwaf_eval (LinearTPWAF Mone (null_vector 2)) x)
+                    (eval_nn_multiple (r:=2) nn (tpwaf_eval (LinearTPWAF Mone (null_vector 2)) x)))))).
+Proof.
+    intros x1 x2 x nn Hx1 Hx2 Hx.
+    unfold monotonicity_1d_postcondition_helper.
+    unfold tpwaf_eval; simpl.
+    unfold netSat_monotonicity_1d_M.
+    rewrite Mplus_null_vector.
+    unfold Mmult, toRS.
+    unfold coeff_colvec.
+    rewrite (coeff_mat_bij _ _ 0 0); try lia.
+    unfold sum_n, sum_n_m, Iter.iter_nat; simpl.
+    unfold coeff_mat at 1; simpl.
+    rewrite (mult_zero_l (K:=Q_RSOPMD)).
+    unfold coeff_mat at 1; simpl.
+    rewrite (mult_zero_l (K:=Q_RSOPMD)).
+    repeat rewrite (plus_zero_l (G:=Q_RSOPMD)).
+    unfold coeff_mat at 1; simpl.
+    unfold colvec_concat at 1.
+    unfold Mplus at 1; rewrite coeff_mat_bij; try lia.
+    unfold extend_colvec_at_bottom at 1, mk_colvec at 1; rewrite coeff_mat_bij; try lia; simpl.
+    unfold extend_colvec_on_top at 1, mk_colvec at 1; rewrite coeff_mat_bij; try lia; simpl.
+    unfold colvec_concat at 1, coeff_colvec at 1.
+    unfold Mplus at 1; rewrite coeff_mat_bij; try lia.
+    unfold extend_colvec_at_bottom at 1, mk_colvec at 1; rewrite coeff_mat_bij; try lia; simpl.
+    unfold extend_colvec_on_top at 1; unfold mk_colvec at 2; rewrite coeff_mat_bij; try lia; simpl.
+    rewrite (plus_zero_l (G:=Q_RSOPMD)).
+    rewrite (plus_zero_r (G:=Q_RSOPMD)).
+    assert (Hhelp: forall v, v = x1 -> nn_eval nn v = nn_eval nn x1). intros v Hv; rewrite Hv; reflexivity.
+    rewrite (Hhelp (mk_colvec 1 _)); clear Hhelp.
+    * unfold coeff_mat at 1; simpl.
+        rewrite (mult_one_l (K:=Q_RSOPMD)).
+        unfold colvec_concat at 1.
+        unfold Mplus at 1; rewrite coeff_mat_bij; try lia.
+        unfold extend_colvec_at_bottom at 1, mk_colvec at 1; rewrite coeff_mat_bij; try lia; simpl.
+        unfold extend_colvec_on_top at 1; unfold mk_colvec at 1; rewrite coeff_mat_bij; try lia; simpl.
+        rewrite (plus_zero_l (G:=Q_RSOPMD)).
+        rewrite (plus_zero_r (G:=Q_RSOPMD)).
+        unfold coeff_colvec at 2, colvec_concat at 1.
+        unfold Mplus at 1; rewrite coeff_mat_bij; try lia.
+        unfold extend_colvec_at_bottom at 1, mk_colvec at 1; rewrite coeff_mat_bij; try lia; simpl.
+        unfold extend_colvec_on_top at 1, mk_colvec at 1; rewrite coeff_mat_bij; try lia; simpl.
+        rewrite (plus_zero_l (G:=Q_RSOPMD)).
+        unfold coeff_colvec at 2, colvec_concat at 1.
+        unfold Mplus at 1; rewrite coeff_mat_bij; try lia.
+        unfold extend_colvec_at_bottom at 1, mk_colvec at 1; rewrite coeff_mat_bij; try lia; simpl.
+        unfold extend_colvec_on_top at 1, mk_colvec at 3; rewrite coeff_mat_bij; try lia; simpl.
+        rewrite (plus_zero_r (G:=Q_RSOPMD)).
+        assert (Hhelp: forall v, v = x2 -> nn_eval nn v = nn_eval nn x2). intros v Hv; rewrite Hv; reflexivity.
+        rewrite (Hhelp (mk_colvec 1 _)); clear Hhelp.
+        - unfold mult, plus; simpl.
+          unfold coeff_colvec, coeff_mat, coeff_Tn.
+          remember (QDEP_le _ _) as comp_res.
+          destruct comp_res.
+          * symmetry in Heqcomp_res; symmetry.
+            apply (ax_real_leq_true Q_RSOPMD); apply (ax_real_leq_true Q_RSOPMD) in Heqcomp_res.
+            RSOPM_realize; simpl; simpl in Heqcomp_res.
+            lra.
+          * symmetry in Heqcomp_res; symmetry.
+            apply (ax_real_leq_false Q_RSOPMD); apply (ax_real_leq_false Q_RSOPMD) in Heqcomp_res.
+            RSOPM_realize; simpl; simpl in Heqcomp_res.
+            lra.
+        - rewrite Hx2.
+          unfold mk_colvec.
+          apply mk_matrix_ext; intros i j Hi Hj.
+          destruct i; destruct j; try lia.
+          unfold coeff_colvec.
+          unfold Mplus; repeat (rewrite coeff_mat_bij; try lia).
+          do 2 rewrite (plus_zero_r (G:=Q_RSOPMD)).
+          unfold Mone; repeat (rewrite coeff_mat_bij; try lia); simpl.
+          rewrite (mult_one_l (K:=Q_RSOPMD)).
+          rewrite (mult_zero_l (K:=Q_RSOPMD)).
+          rewrite (plus_zero_l (G:=Q_RSOPMD)).
+          reflexivity.        
+    * rewrite Hx1.
+      unfold mk_colvec.
+      apply mk_matrix_ext; intros i j Hi Hj.
+      destruct i; destruct j; try lia.
+      unfold coeff_colvec.
+      unfold Mplus; repeat (rewrite coeff_mat_bij; try lia).
+      do 2 rewrite (plus_zero_r (G:=Q_RSOPMD)).
+      unfold Mone; repeat (rewrite coeff_mat_bij; try lia); simpl.
+      rewrite (mult_one_l (K:=Q_RSOPMD)).
+      rewrite (mult_zero_l (K:=Q_RSOPMD)).
+      rewrite (plus_zero_r (G:=Q_RSOPMD)).
+      reflexivity. 
+Qed.
+
+Lemma monotonicity_1d_correct:
+    forall (nn: TPWANNSequential (RSOPM:=Q_RSOPMD)),
+        is_monotone_1d nn <-> nn_satisfies_nndh nn NNDH_monotonicity_1d.
+Proof.
+    intros nn.
+    split; intros H.
+    * unfold is_monotone_1d in H.
+      unfold nn_satisfies_nndh.
+      unfold NNDH_monotonicity_1d.
+      intros x HxW.
+      pose proof (colvec_split 1 1 x) as Hsplit.
+      destruct Hsplit as [x1 [x2 [Hx1 [Hx2 Hxconcat]]]].
+      specialize (H x1 x2).
+      pose proof monotonicity_1d_postcondition as Hpostcondition.
+      specialize (Hpostcondition x1 x2 x nn Hx1 Hx2 Hxconcat).
+      unfold monotonicity_1d_postcondition_helper in Hpostcondition.
+      rewrite Hpostcondition in H.
+      apply H.
+      apply W_monotonicity_1d_correct.
+      rewrite Hxconcat in HxW.
+      apply HxW.
+    * unfold is_monotone_1d.
+      intros x1 x2 Hpre.
+      unfold nn_satisfies_nndh, NNDH_monotonicity_1d in H.
+      apply W_monotonicity_1d_correct in Hpre.
+      specialize (H (colvec_concat x1 x2) Hpre).
+      rewrite (monotonicity_1d_postcondition _ _ (colvec_concat x1 x2)); last reflexivity.
+      - unfold monotonicity_1d_postcondition_helper.
+        apply H.
+      - rewrite <- (mk_matrix_bij 0 x1).
+        apply mk_matrix_ext.
+        intros i j Hi Hj.
+        induction i; induction j; try lia.
+        unfold colvec_concat, coeff_colvec, Mplus, extend_colvec_at_bottom, extend_colvec_on_top, mk_colvec, coeff_colvec.
+        repeat (rewrite coeff_mat_bij; try lia); simpl.
+        rewrite (plus_zero_r (G:=Q_RSOPMD)); reflexivity.
+      - rewrite <- (mk_matrix_bij 0 x2).
+        apply mk_matrix_ext.
+        intros i j Hi Hj.
+        induction i; induction j; try lia.
+        unfold colvec_concat, coeff_colvec, Mplus, extend_colvec_at_bottom, extend_colvec_on_top, mk_colvec, coeff_colvec.
+        repeat (rewrite coeff_mat_bij; try lia); simpl.
+        rewrite (plus_zero_l (G:=Q_RSOPMD)); reflexivity.
+Qed.
+
+Lemma is_monotone_1d_verification:
+  forall nn,
+    verify_hyperporperty nn NNDH_monotonicity_1d = true -> is_monotone_1d nn.
+Proof.
+  intros nn H.
+  apply monotonicity_1d_correct.
+  apply verify_hyperporperty_correct.
+  apply H.
+Qed.
+
+End Monotonicity1DHyperpropery.
+
+Section ExampleVerification1.
+    
+Definition example1_weights1: matrix (T:=Q_RSOPMD) 1 1 :=
+    [[toQDEP 1%Q]].
+
+Definition example1_biases1: matrix 1 1 :=
+    [[toQDEP 0.1%Q]].
+
+Definition example_nn1 := 
+    (NNLinear example1_weights1 example1_biases1 
+    (NNReLU
+    (NNOutput (output_dim:=1)))).
+
+Theorem example1: 
+  is_monotone_1d example_nn1.
+Proof.
+  apply is_monotone_1d_verification.
+  compute; reflexivity.
+Qed.
+
+End ExampleVerification1. 
+
+Section ExampleVerification2.
+    
+Definition example2_weights1: matrix (T:=Q_RSOPMD) 2 1 :=
+    [[toQDEP 1%Q], [toQDEP 2%Q]].
+
+Definition example2_biases1: matrix 2 1 :=
+    [[toQDEP 0.5%Q], [toQDEP 0.3%Q]].
+
+Definition example2_weights2: matrix (T:=Q_RSOPMD) 1 2 :=
+    [[toQDEP 1%Q, toQDEP 2%Q]].
+
+Definition example2_biases2: matrix 1 1 :=
+    [[toQDEP 0.3%Q]].
+
+Definition example_nn2 := 
+    (NNLinear example2_weights1 example2_biases1 
+    (NNReLU
+    (NNLinear example2_weights2 example2_biases2
+    (NNReLU
+    (NNOutput (output_dim:=1)))))).
+
+Theorem example2: 
+  is_monotone_1d example_nn2.
+Proof.
+  apply is_monotone_1d_verification.
+  compute; reflexivity.
+Qed.
+
+End ExampleVerification2.
+
+Section ExampleVerification3.
+    
+Definition example3_weights1: matrix (T:=Q_RSOPMD) 3 1 :=
+    [[toQDEP 1%Q], [toQDEP 2%Q], [toQDEP 3%Q]].
+
+Definition example3_biases1: matrix 3 1 :=
+    [[toQDEP 0.1%Q], [toQDEP 0.2%Q], [toQDEP 0.3%Q]].
+
+Definition example3_weights2: matrix (T:=Q_RSOPMD) 4 3 :=
+    [[toQDEP 11%Q, toQDEP 12%Q, toQDEP 13%Q],
+     [toQDEP 21%Q, toQDEP 22%Q, toQDEP 23%Q],
+     [toQDEP 31%Q, toQDEP 32%Q, toQDEP 33%Q],
+     [toQDEP 41%Q, toQDEP 42%Q, toQDEP 43%Q]].
+
+Definition example3_biases2: matrix 4 1 :=
+    [[toQDEP 0.2%Q], [toQDEP 0.3%Q], [toQDEP 0.4%Q], [toQDEP 0.5%Q]].
+
+Definition example3_weights3: matrix (T:=Q_RSOPMD) 1 4 :=
+    [[toQDEP 10%Q, toQDEP 20%Q, toQDEP 30%Q, toQDEP 40%Q]].
+
+Definition example3_biases3: matrix 1 1 :=
+    [[toQDEP 0.1%Q]].
+
+Definition example_nn3 := 
+    (NNLinear example3_weights1 example3_biases1 
+    (NNReLU
+    (NNLinear example3_weights2 example3_biases2
+    (NNReLU
+    (NNLinear example3_weights3 example3_biases3 
+    (NNReLU
+    (NNOutput (output_dim:=1)))))))).
+
+(* Takes around 20 minutes *)
+Theorem example3: 
+  is_monotone_1d example_nn3.
+Proof.
+  apply is_monotone_1d_verification.
+  compute; reflexivity.
+Qed.
+
+End ExampleVerification3.
+
