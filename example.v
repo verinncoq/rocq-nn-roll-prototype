d214882ed541a62e1f680a191318d142957eb646
Zawuza <andreizawuza@gmail.com>
2025-11-14 16:01:43 +0100

    Improve negative example: - is_monotone_1d_verification is now iff - A larger network and shorter proof for example 4

--- example.v
+++ example.v
@@ -215,12 +215,12 @@ Qed.
 
 Lemma is_monotone_1d_verification:
   forall nn,
-    verify_hyperporperty nn NNDH_monotonicity_1d = true -> is_monotone_1d nn.
+    verify_hyperporperty nn NNDH_monotonicity_1d = true <-> is_monotone_1d nn.
 Proof.
-  intros nn H.
-  apply monotonicity_1d_correct.
-  apply verify_hyperporperty_correct.
-  apply H.
+  intro nn.
+  rewrite monotonicity_1d_correct.
+  rewrite verify_hyperporperty_correct.
+  apply iff_refl.
 Qed.
 
 End Monotonicity1DHyperpropery.
@@ -309,42 +309,44 @@ Definition example_nn3 :=
     (NNReLU
     (NNOutput (output_dim:=1)))))))).
 
-(* Takes around 20 minutes *)
+(* Takes around 20 minutes 
 Theorem example3: 
   is_monotone_1d example_nn3.
 Proof.
   apply is_monotone_1d_verification.
   compute; reflexivity.
-Qed.
+Qed. *)
 
 End ExampleVerification3.
 
 Section ExampleVerification4.
-(*negative example*)
 
-Definition example4_weights1: matrix (T:=Q_RSOPMD) 1 1 :=
-    [[toQDEP (-1)%Q]].
+Definition example4_weights1: matrix (T:=Q_RSOPMD) 3 1 :=
+    [[toQDEP (-1)%Q], [toQDEP 1%Q], [toQDEP 0.7%Q]].
 
-Definition example4_biases1: matrix 1 1 :=
-    [[toQDEP (0.1)%Q]].
+Definition example4_biases1: matrix 3 1 :=
+    [[toQDEP 0.1%Q], [toQDEP 0.25%Q], [toQDEP 0%Q]].
+
+Definition example4_weights2: matrix (T:=Q_RSOPMD) 1 3 :=
+    [[toQDEP 0.66%Q, toQDEP (-0.3)%Q, toQDEP 0.99%Q]].
+
+Definition example4_biases2: matrix 1 1 :=
+    [[toQDEP 0.1%Q]].
 
 Definition example_nn4 := 
     (NNLinear example4_weights1 example4_biases1 
     (NNReLU
-    (NNOutput (output_dim:=1)))).
+    (NNLinear example4_weights2 example4_biases2
+    (NNReLU
+    (NNOutput (output_dim:=1)))))).
 
-(*give counterexample x1=0, x2=1*)
 Theorem example4_not_monotone :
   ~ is_monotone_1d example_nn4.
 Proof.
-unfold not.
-intros H.
-set (x1 := mk_colvec (RSOPM:= Q_RSOPMD) 1 (fun _ => 0%RS)).
-set (x2 := mk_colvec (RSOPM:= Q_RSOPMD) 1 (fun _ => 1%RS)).
-specialize (H x1 x2).
-compute in H.
-discriminate H.
-reflexivity.
+  intro Hcontra.
+  apply is_monotone_1d_verification in Hcontra.
+  compute in Hcontra.
+  discriminate.
 Qed.
 
 End ExampleVerification4. 
